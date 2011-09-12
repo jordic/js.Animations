ed2092a3540c961ccdafd269ee0bee35c6870b93
Jordi Collell <jordic@gmail.com>
2011-09-12 09:13:03 +0200

    getters i setters en format val() tipus jquery

--- js/Sprite.js
+++ js/Sprite.js
@@ -62,16 +62,21 @@
 		
 		/* x, y, z setters */
 		this.x = function(v) {
+			
+			if(!v) return that._x
 			pt.css('left', v); 
 			that._x = v;
 			return that; 
 			}
 		this.y = function(v) { 
+		  
+		  if(!v) return that._y
 			pt.css('top', v); 
 			that._y = v;
 			return that; 
 			}
 		this.z = function(v) { 
+		  if(!v) return that._z
 			pt.css('z-index', v); 
 			that._z = v;
 			return that; 
@@ -97,28 +102,41 @@
 	
 
 	
-	$.extend(jQuery.GT.Sprite.prototype, {
-		// s'han d'implementar metodes per mozilla i ie.... :P
-		rotate: function(angle) {
-		  this._rotate = angle;
-		  this.apply_transform();
-		}, 
-		scale: function(valx, valy) {
-			if(!valy)
-				valy = valx
-			this._scaleX = valx;
-			this._scaleY = valy;
-			this.apply_transform();
-		},
-		apply_transform: function()
-		{
-		  this.css('-webkit-transform', 'rotate('+ this._rotate +'deg) scale('+this._scaleX+','+ this._scaleY + ')')
-		},
-		origin: function(x,y) {
-			this.css('-webkit-transform-origin', x + 'px')
-		}
-		
-	})
+$.extend(jQuery.GT.Sprite.prototype, {
+// s'han d'implementar metodes per mozilla i ie.... :P
+rotate: function(angle) {
+  if(!angle) return this._rotate;
+  this._rotate = angle;
+  this.apply_transform();
+}, 
+scale: function(valx, valy) {
+  if(!valy)
+		valy = valx
+	this._scaleX = valx;
+	this._scaleY = valy;
+	this.apply_transform();
+},
+scaleX : function(v)
+{
+  if(!v) return this._scaleX;
+  this._scaleX = v;
+	this.apply_transform();
+},
+scaleY: function(v)
+{
+  if(!v) return this._scaleY;
+  this._scaleY = v;
+	this.apply_transform();
+},
+apply_transform: function()
+{
+  this.css('-webkit-transform', 'rotate('+ this._rotate +'deg) scale('+this._scaleX+','+ this._scaleY + ')')
+},
+origin: function(x,y) {
+	this.css('-webkit-transform-origin', x + 'px');
+}
+
+})
 	
 	
 }());
